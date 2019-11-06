beb2dbdef2545470d9910aa33a8115e529e9266e
yuhang4 <yuhang4@uw.edu>
2019-11-06 08:38:16 -0800

    add method to datahandle.py

--- code/datahandle.py
+++ code/datahandle.py
@@ -14,13 +14,29 @@ useful_listing_df = raw_listing_df[['id', 'listing_url', 'name', 'description', 
 'minimum_nights', 'maximum_nights', 'review_scores_rating', 'review_scores_accuracy', \
 'review_scores_cleanliness', 'requires_license', 'reviews_per_month']]
 
-def showHead():
+def get_raw_dataframe():
+    return useful_listing_df
+
+def show_raw_head():
+    '''
+    Return the head of useful_listing_df dataframe
+    '''
     return useful_listing_df.head()
 
-def test():
-    useful_listing_df.head()
+def basic_recommend_search(zipcode, accommodates = None, price = None, review_scores_rating = None):
+    '''
+    Recommend house by using basic 4 parameters (zipcode, accommodates, price, overall_rating)
+    Parameter: String zipcode, Integer accommodates, String price, Integer review_scores_rating
+    Return: dataframe, where zipcode == input, accommodates >= input, price <= input, scores <= input
+    '''
+    temp_df = useful_listing_df[useful_listing_df['zipcode'] == zipcode]
+    if accommodates != None:
+        temp_df = temp_df[temp_df['accommodates'] >= accommodates]
+    if price != None:
+        temp_df = temp_df[temp_df['price'] <= price]
+    return temp_df
 
 if __name__ == "__main__":
-    test()
+    get_raw_dataframe()
 
 
