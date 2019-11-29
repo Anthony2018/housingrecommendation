1250555a81dc0d9e58aa6a620f15ad0d1f3267aa
yuhang4 <yuhang4@uw.edu>
2019-11-29 12:41:15 -0800

    new datahandle.py

--- code/datahandle.py
+++ code/datahandle.py
@@ -3,9 +3,10 @@ import numpy as np
 
 listing_url = "https://raw.githubusercontent.com/adonis-wyc/housingrecommendation/master/data/listings.csv"
 review_url = "https://raw.githubusercontent.com/adonis-wyc/housingrecommendation/master/data/reviews.csv"
-raw_listing_df = pd.read_csv(listing_url)
-raw_review_df = pd.read_csv(review_url)
+raw_listing_df = pd.read_csv(listing_url) # raw listing dataframe with 96 columns
+raw_review_df = pd.read_csv(review_url) # raw review dataframe with 6 columns
 
+# Select needed 38 columns into useful_listing_df
 useful_listing_df = raw_listing_df[['id', 'listing_url', 'name', 'description', 'thumbnail_url',\
 'host_id', 'host_url', 'host_name', 'host_since', 'host_response_rate', 'host_is_superhost',\
 'host_listings_count', 'host_identity_verified', 'street', 'zipcode', 'latitude', 'longitude',\
@@ -14,7 +15,14 @@ useful_listing_df = raw_listing_df[['id', 'listing_url', 'name', 'description', 
 'minimum_nights', 'maximum_nights', 'review_scores_rating', 'review_scores_accuracy', \
 'review_scores_cleanliness', 'requires_license', 'reviews_per_month']]
 
+pd.set_option('mode.chained_assignment', None)
+useful_listing_df['host_is_superhost'] = raw_listing_df.host_is_superhost == 't'
+useful_listing_df['host_identity_verified'] = raw_listing_df.host_identity_verified == 't'
+
 def get_raw_dataframe():
+    '''
+    Return the raw useful_listing  dataframe
+    '''
     return useful_listing_df
 
 def show_raw_head():
@@ -23,17 +31,74 @@ def show_raw_head():
     '''
     return useful_listing_df.head()
 
-def basic_recommend_search(zipcode, accommodates = None, price = None, review_scores_rating = None):
+def primary_recommend_search(zipcode: str, accommodates: int = None, price: str = None, \
+    review_scores_rating: int = None, host_is_superhost: bool = None, host_identity_verified: bool = None, \
+    room_type: str = None, bathrooms: float = None, bedrooms: int = None, cleaning_fee: str = None, \
+    guests_included: int = None, nights: int = None, review_scores_cleanliness: int = None, \
+    review_scores_accuracy: int = None, requires_license: bool = None, \
+    sorting_list: list = ['review_scores_rating', 'price'], sorting_way: list = [False, True]):
     '''
-    Recommend house by using basic 4 parameters (zipcode, accommodates, price, overall_rating)
-    Parameter: String zipcode, Integer accommodates, String price, Integer review_scores_rating
-    Return: dataframe, where zipcode == input, accommodates >= input, price <= input, scores <= input
+    Recommend house by using many more parameters than basic search, but all of these parameters are set to None
+    defaultly except for zipcode, so you could get result by enter at least one parameter zipcode
+    If you do not want any parameter take effects, just let this parameter as none
+    Default sorting_list = ['review_scores_rating', 'price'], sorting_way = [False, True] for ascending
+    @Return: dataframe
     '''
     temp_df = useful_listing_df[useful_listing_df['zipcode'] == zipcode]
     if accommodates != None:
         temp_df = temp_df[temp_df['accommodates'] >= accommodates]
     if price != None:
         temp_df = temp_df[temp_df['price'] <= price]
+    if review_scores_rating != None:
+        temp_df = temp_df[temp_df['review_scores_rating'] >= review_scores_rating]
+    if host_is_superhost != None:
+        temp_df = temp_df[temp_df['host_is_superhost'] == host_is_superhost]
+    if host_identity_verified != None:
+        temp_df = temp_df[temp_df['host_identity_verified'] == host_identity_verified]
+    if room_type != None:
+        temp_df = temp_df[temp_df['room_type'] == room_type]
+    if bathrooms != None:
+        temp_df = temp_df[temp_df['bathrooms'] >= bathrooms]
+    if bedrooms != None:
+        temp_df = temp_df[temp_df['bedrooms'] >= bedrooms]
+    if cleaning_fee != None:
+        temp_df = temp_df[(temp_df['cleaning_fee'] <= cleaning_fee) | (temp_df['cleaning_fee'].isna())]
+    if guests_included != None:
+        temp_df = temp_df[temp_df['guests_included'] >= guests_included]
+    if nights != None:
+        temp_df = temp_df[(temp_df['minimum_nights'] <= nights) & (temp_df['maximum_nights'] >= nights)]
+    if review_scores_cleanliness != None:
+        temp_df = temp_df[temp_df['review_scores_cleanliness'] >= review_scores_cleanliness]
+    if review_scores_accuracy != None:
+        temp_df = temp_df[temp_df['review_scores_accuracy'] >= review_scores_accuracy]
+    if requires_license != None:
+        temp_df = temp_df[temp_df['requires_license'] == requires_license]
+    temp_df = temp_df.sort_values(sorting_list, ascending = sorting_way)
+    return temp_df
+
+
+def basic_recommend_search(zipcode: str, accommodates: int = None, price: str = None, \
+    review_scores_rating: int = None, sorting_list: list = ['review_scores_rating', 'price'], \
+    sorting_way: list = [False, True]):
+    '''
+    Recommend house by using basic 4 parameters (zipcode, accommodates, price, overall_rating)
+    and sorting by 2 parameters (sorting_list, sorting_way), and these two list should have same length
+    The default sorting_list = ['review_scores_rating', 'price'], default sorting_way = [False, True] for ascending
+
+    @Parameter: String zipcode, Integer accommodates, String price, Integer review_scores_rating
+
+    @Sorting parameter: List sorting_list, List sorting_way
+
+    @Return: useful_listing_df, where zipcode == input, accommodates >= input, price <= input, scores >= input
+    '''
+    temp_df = useful_listing_df[useful_listing_df['zipcode'] == zipcode]
+    if accommodates != None:
+        temp_df = temp_df[temp_df['accommodates'] >= accommodates]
+    if price != None:
+        temp_df = temp_df[temp_df['price'] <= price]
+    if review_scores_rating != None:
+        temp_df = temp_df[temp_df['review_scores_rating'] >= review_scores_rating]
+    temp_df = temp_df.sort_values(sorting_list, ascending = sorting_way)
     return temp_df
 
 if __name__ == "__main__":
