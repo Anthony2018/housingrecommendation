6d346cd3373a1d98ed89b3c145dda206db5047cf
yuhang4 <yuhang4@uw.edu>
2019-12-05 22:19:50 -0800

    add html server

---
+++ code/htmlserver/app.py
@@ -0,0 +1,59 @@
+import datahandle as dh
+import pandas as pd
+import json
+import requests
+from geojson import Point, Feature
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+app = Flask(__name__)
+
+def get_marker_points(df):
+    points = []
+    for index, row in df.iterrows():
+        point = Point([row['longitude'], row['latitude']])
+        properties = {
+            'url': str(row['listing_url']),
+            'title': row['name'],
+            'price': row['price'],
+            'accommodates': row['accommodates'],
+            'room_type': row['room_type'],
+            'thumbnail_url': str(row['picture_url'])
+        }
+        feature = Feature(geometry = point, properties = properties)
+        points.append(feature)
+    return points
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        zipcode = request.form.get('zipcode')
+        nights = request.form.get('nights')
+        price = request.form.get('price')
+        accommodates = request.form.get('accommodates')
+        room_type = request.form.get('room_type')
+        score = request.form.get('score')
+        is_super_host = request.form.get('super_host')
+        is_verified_host = request.form.get('verified_host')
+        is_need_license = request.form.get('need_license')
+
+        nights = None if nights == "" else int(nights)
+        price =  None if price == "" else "$" + price
+        accommodates = None if accommodates == "" else int(accommodates)
+        room_type =  None if room_type == "" else room_type
+        score = None if score == "" else int(score)
+        is_super_host = True if is_super_host == "True" else None
+        is_verified_host = True if is_verified_host == "True" else None
+        is_need_license = True if is_need_license == "True" else None
+
+        df_table = dh.primary_recommend_search(zipcode, accommodates, price, score, is_super_host,\
+            is_verified_host, room_type, None, None, None, None, nights, None, None, is_need_license)
+        
+        if df_table.empty:
+            return render_template('not_found.html')
+
+        points_list = get_marker_points(df_table)
+
+        return render_template('recommendation.html', points_list = points_list,\
+            house_list = df_table)
+    return render_template('index.html')
